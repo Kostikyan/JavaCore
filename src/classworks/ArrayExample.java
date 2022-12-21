9a8b9e62999ad212ac098b6ef7585f4879c74c55
Kostikyan <kostikyanv@gmail.com>
2022-12-21 22:21:34 +0400

    Update code

--- src/classworks/ArrayExample.java
+++ src/classworks/ArrayExample.java
@@ -3,11 +3,10 @@ package classworks;
 public class ArrayExample {
 
     public static void main(String[] args) {
-        int[] nums = {33,44,-2,6612,77342};
+        int[] nums = {33, 44, -2, 6612, 77342};
         for (int i = 0; i < 4; i++) {
             System.out.print(nums[i] + " ");
         }
-
         int[] myArray = new int[10];
         //add nums to array
         for (int i = 0; i < 10; i++) {
--- src/homeworks/homework1/IfForExample.java
+++ src/homeworks/homework1/IfForExample.java
@@ -6,26 +6,37 @@ public class IfForExample {
         int a, b;
         a = 50;
         b = 80;
+
         // max int
         if (a > b) {
             System.out.println("max: " + a);
         } else {
             System.out.println("max: " + b);
         }
+
         // all numbers from 50 to 80
-        for (a = 50; a < b + 1 ; a++) {
-            System.out.println(a);
+        int i;
+        if (a > b) {
+            for (i = b; i <= a; i++) {
+                System.out.print(i + " ");
+            }
+        } else {
+            for (i = a; i <= b; i++) {
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
+
         // ints a and b to char
         char inta = (char) a;
         char intb = (char) b;
         System.out.println("a: " + inta);
         System.out.println("b: " + intb);
+
         // if a = b
-        if (a == b){
+        if (a == b) {
             System.out.println("a equals b");
-        }
-        else{
+        } else {
             System.out.println("a is not equal to b");
         }
     }

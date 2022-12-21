d5a0dd3cd97f4df9a73af4052ff60a7653c65a3c
Kostikyan <kostikyanv@gmail.com>
2022-12-21 23:16:41 +0400

    Update code

---
+++ src/homeworks/homework2/ArrayUtil.java
@@ -0,0 +1,35 @@
+package homeworks.homework2;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+
+        int[] nums = {24, 556, 82, 101, 3352, 226, 171, 616, 102, 310};
+
+        // all elements
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(i + ")" + nums[i]);
+        }
+
+        // max num in nums
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+
+            }
+        }
+        System.out.println(max);
+
+        // min num in nums
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        System.out.println(min);
+
+
+    }
+}
---
+++ src/homeworks/homework2/FIgurePaiter2.java
@@ -0,0 +1,42 @@
+package homeworks.homework2;
+
+
+public class FIgurePaiter2 {
+
+    public static void main(String[] args) {
+        // FIGURE PAINTER 1
+        System.out.println("FIRST FIGURE: ");
+        for (int i = 4; i > 0; i--) {
+            for (int j = 7; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        // FIGURE PAINTER 2
+        System.out.print("SECOND FIGURE: ");
+        for (int i = 0; i <= 4; i++) {
+            for (int j = i; j < 4; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 3; i > 0; i--) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}

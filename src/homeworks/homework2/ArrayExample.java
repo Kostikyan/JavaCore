daaed20e2242fe1e7b7e2c8274b00ee0a7c827c2
Kostikyan <kostikyanv@gmail.com>
2022-12-22 19:32:36 +0400

    Update code

---
+++ src/homeworks/homework2/ArrayExample.java
@@ -0,0 +1,19 @@
+package homeworks.homework2;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{11,27,53,34,51,0,123,51,45};
+        int n = 1; // any number (not 0)
+        for (int i = 0; i < numbers.length; i++) {
+            int ifzero = numbers[i] / n;
+            if(ifzero != 0){
+                System.out.println(numbers[i]);
+            }
+
+        }
+
+    }
+
+}

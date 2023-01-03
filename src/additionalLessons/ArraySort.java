d78a6ebe09da94ececbdc4a730ac8661562779c9
Kostikyan <kostikyanv@gmail.com>
2023-01-03 22:07:44 +0400

    Add class ArraySort.java in package "additionalLessons"

---
+++ src/additionalLessons/ArraySort.java
@@ -0,0 +1,60 @@
+package additionalLessons;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraySort {
+
+    public static void main(String[] args) {
+
+        Integer[] numArr = {1, 3, 123, 42, 125, 43, -1, -235, 12321, 65748};
+        int arrLen = numArr.length;
+
+        normalArray:
+        {
+            int i = 0;
+            System.out.print("Original Array: ");
+            for (int x : numArr) {
+                i++;
+                System.out.print(x);
+                if (i != arrLen) {
+                    System.out.print(", ");
+                }
+            }
+        }
+
+        sortedArray:
+        {
+            int n = 0;
+            Arrays.sort(numArr);
+            System.out.print("\nSorted Array: ");
+            for (int x : numArr) {
+                n++;
+                System.out.print(x);
+                if (n != arrLen) {
+                    System.out.print(", ");
+                }
+            }
+        }
+
+        revSortedArray:
+        {
+            int n = 0;
+            Arrays.sort(numArr, Collections.reverseOrder());
+            System.out.print("\nReverse Sorted Array: ");
+            for (int x : numArr) {
+                n++;
+                System.out.print(x);
+                if (n != arrLen) {
+                    System.out.print(", ");
+                }
+            }
+        }
+        maxAndMinNumInArray: {
+            Arrays.sort(numArr);
+            System.out.println("\nMax num in Array: " + numArr[numArr.length-1]);
+            System.out.println("Min num in Array: " + numArr[0]);
+        }
+    }
+
+}

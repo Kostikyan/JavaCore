da66ee66cd6348ee5e940754f1c48437adf66a30
Kostikyan <kostikyanv@gmail.com>
2023-01-06 17:47:49 +0400

    Crate Calculator

--- src/myJavaProjects/Calculator/Calculator.java
+++ src/myJavaProjects/Calculator/Calculator.java
@@ -20,7 +20,7 @@ public class Calculator implements ActionListener {
     char operator;
 
     Calculator() {
-        
+
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 550);

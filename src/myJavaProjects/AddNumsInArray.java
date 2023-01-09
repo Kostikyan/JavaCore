d4728ea9f5b54a5c09098fa406a27a32caa24da1
Kostikyan <kostikyanv@gmail.com>
2023-01-09 21:57:09 +0400

    update program AddNumsInArray.java

--- src/myJavaProjects/AddNumsInArray.java
+++ src/myJavaProjects/AddNumsInArray.java
@@ -1,25 +1,43 @@
 package myJavaProjects;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AddNumsInArray {
-    public static void main(String args[]) {
-        int x = 0;
-        int[] arr = new int[4];
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        System.out.println("Сколько элементов вы хотите? (не рекомендуется больше 20-и)");
+        int n = scan.nextInt();
+        if(n < 0){
+            System.err.println("Количество Элементов В Массиве Не Может Быть Отрицательным. //error");
+            return;
+        }
+        if(n == 0){
+            System.err.println("Нету Смысла! //error");
+            return;
+        }
+        int x = 0;
+        int[] arr = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("введите число: ");
+            System.out.print("введите " + i + "-е " + "число: ");
             int value = scan.nextInt();
             arr[i] = value;
-            if(i == 3){
-                for (i = 0; i < arr.length; i++){
-                    System.out.println((x++) + ")" + arr[i]);
-                }
+        }
+        System.out.print("Все элементы: ");
+        for (int j : arr) {
+            System.out.print(x++ + ")" + j);
+            if(x != arr.length){
+                System.out.print(", ");
             }
         }
-
+        System.out.println();
+        Arrays.sort(arr);
+        System.out.println("Максимум: " + arr[arr.length-1]);
+        System.out.println("Минимум: " + arr[0]);
+        System.out.println("Сумма: " + Arrays.stream(arr).sum());
 
     }
 

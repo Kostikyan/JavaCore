42164aa4ff1a88cd84e45038b5294159b26d17eb
Kostikyan <kostikyanv@gmail.com>
2023-01-29 11:16:38 +0400

    add ParseIntExample.java

---
+++ src/classworks/ParseIntExample.java
@@ -0,0 +1,13 @@
+package classworks;
+
+public class ParseIntExample {
+
+    public static void main(String[] args)   {
+
+        String numStr = "342";
+        int num = Integer.parseInt(numStr);
+        System.out.println(num * 2);
+
+    }
+
+}

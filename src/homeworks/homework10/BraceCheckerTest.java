b0330d72aae5c22056375f777fb14d6eb94dbd16
Kostikyan <kostikyanv@gmail.com>
2023-01-29 09:29:39 +0400

    optimise homework10

--- src/homeworks/homework10/BraceCheckerTest.java
+++ src/homeworks/homework10/BraceCheckerTest.java
@@ -5,7 +5,7 @@ public class BraceCheckerTest {
     public static void main(String[] args) {
 
         // input text
-        homeworks.homework10.BraceChecker bc = new BraceChecker("Hello from [Java} [] {} () }}{}))");
+        BraceChecker bc = new BraceChecker("Hello from [Java} [] {} () }}{}))");
 
         // checking is correct brackets
         bc.check();

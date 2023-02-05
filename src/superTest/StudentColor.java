be295790fb9e5a7e6b9ea319c6482efd04b7e185
Vahe <120592399+Kostikyan@users.noreply.github.com>
2023-02-05 10:34:48 +0400

    Delete src/superTest directory

--- src/superTest/StudentColor.java
+++
@@ -1,9 +0,0 @@
-package superTest;
-
-public class StudentColor extends Student{
-    String color = null;
-    StudentColor(String n, String s, String c){
-        super(n, s);
-        color = c;
-    }
-}
